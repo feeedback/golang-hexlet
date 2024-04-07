b84f4d2330121ac6820ec09fd77790bd6516effe
Aleksandr Rzhanitsyn <risdeep@gmail.com>
2024-04-08 02:12:32 +0400

    add go-web-development\13

---
+++ go-web-development/13/solution.go
@@ -0,0 +1,56 @@
+// https://ru.hexlet.io/courses/go-web-development/lessons/error-handling/exercise_unit
+package main
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/sirupsen/logrus"
+)
+
+type (
+	SendPushNotificationRequest struct {
+		Message string `json:"message"`
+		UserID  int64  `json:"user_id"`
+	}
+
+	PushNotification struct {
+		Message string `json:"message"`
+		UserID  int64  `json:"user_id"`
+	}
+)
+
+var pushNotificationsQueue []PushNotification
+
+func main() {
+	// BEGIN (write your solution here)
+	webApp := fiber.New(fiber.Config{
+		ReadTimeout:  300 * time.Millisecond,
+		WriteTimeout: 300 * time.Millisecond,
+	})
+
+	webApp.Use(recover.New())
+	// END
+	webApp.Get("/", func(c *fiber.Ctx) error {
+		return c.SendStatus(200)
+	})
+
+	webApp.Post("/push/send", func(c *fiber.Ctx) error {
+		var req SendPushNotificationRequest
+		if err := c.BodyParser(&req); err != nil {
+			// BEGIN (write your solution here)
+			return c.Status(fiber.StatusBadRequest).SendString("Invalid JSON")
+			// END
+		}
+
+		pushNotificationsQueue = append(pushNotificationsQueue, PushNotification(req))
+		if len(pushNotificationsQueue) > 3 {
+			panic("Queue is full")
+		}
+
+		return c.SendStatus(fiber.StatusOK)
+	})
+
+	logrus.Fatal(webApp.Listen(":8080"))
+}
---
+++ go-web-development/13/solution_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPractice(t *testing.T) {
+	t.Run("bad_request", func(t *testing.T) {
+		tr := require.New(t)
+
+		testRequest(tr, "{", fiber.StatusBadRequest, "Invalid JSON")
+	})
+
+	t.Run("panics", func(t *testing.T) {
+		tr := require.New(t)
+
+		testRequest(tr, `{"user_id":1,"message":"Hello world!"}`, fiber.StatusOK, "OK")
+		testRequest(tr, `{"user_id":2,"message":"Hello world2!"}`, fiber.StatusOK, "OK")
+		testRequest(tr, `{"user_id":3,"message":"Hello world3!"}`, fiber.StatusOK, "OK")
+		testRequest(tr, `{"user_id":4,"message":"Hello world4!"}`, fiber.StatusInternalServerError, "Queue is full")
+	})
+}
+
+func testRequest(r *require.Assertions, body string, wantCode int, wantBody string) {
+	var bodyReader io.Reader
+	if body != "" {
+		bodyReader = strings.NewReader(body)
+	}
+	req, err := http.NewRequest(http.MethodPost, "http://localhost:8080/push/send", bodyReader)
+	r.NoError(err)
+
+	req.Header.Set("Content-Type", "application/json")
+
+	httpClient := &http.Client{}
+	resp, err := httpClient.Do(req)
+	r.NoError(err)
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	r.NoError(err)
+
+	r.Equal(wantCode, resp.StatusCode)
+	r.Equal(wantBody, string(bodyBytes))
+}
